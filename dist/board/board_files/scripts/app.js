96f25ef969ff84eb8b3ee4a22a26e0c5c24491c5
Tomoya Koyanagi <tomk79@gmail.com>
2016-06-12 16:08:03 +0900

    Keypress: initialize

--- dist/board/board_files/scripts/app.js
+++ dist/board/board_files/scripts/app.js
@@ -4752,7 +4752,8 @@ window.app = new (function(){
 	var Promise = require('es6-promise').Promise;
 	var utils79 = require('utils79');
 	var it79 = require('iterate79');
-	var socket;
+	var socket,
+		_Keypress;
 
 	/**
 	 * 初期化
@@ -4794,9 +4795,55 @@ window.app = new (function(){
 					}
 				);
 			}); })
-			// .then(function(){ return new Promise(function(rlv, rjt){
-			// 	rlv();
-			// }); })
+			.then(function(){ return new Promise(function(rlv, rjt){
+				// キーボードイベントセット
+				var cmdKeyName = (function(ua){
+					// console.log(ua);
+					var idxOf = ua.indexOf( 'Mac OS X' );
+					if( idxOf >= 0 ){
+						return 'cmd';
+					}
+					return 'ctrl';
+				})(window.navigator.userAgent);
+				// console.log(cmdKeyName);
+
+				_Keypress = new window.keypress.Listener();
+				_this.Keypress = _Keypress;
+				_Keypress.simple_combo("backspace", function(e) {
+					switch(e.target.tagName.toLowerCase()){
+						case 'input': case 'textarea':
+						return true; break;
+					}
+					e.preventDefault();
+				});
+				_Keypress.simple_combo("delete", function(e) {
+					switch(e.target.tagName.toLowerCase()){
+						case 'input': case 'textarea':
+						return true; break;
+					}
+					e.preventDefault();
+				});
+				_Keypress.simple_combo("escape", function(e) {
+					switch(e.target.tagName.toLowerCase()){
+						case 'input': case 'textarea':
+						return true; break;
+					}
+					e.preventDefault();
+				});
+				_Keypress.simple_combo("enter", function(e) {
+					switch(e.target.tagName.toLowerCase()){
+						case 'input': case 'textarea':
+						return true; break;
+					}
+					e.preventDefault();
+					alert('enter');
+				});
+				// _Keypress.simple_combo(cmdKeyName+" x", function(e) {
+				// 	px.message('cmd x');
+				// 	e.preventDefault();
+				// });
+				rlv();
+			}); })
 			.then(function(){ return new Promise(function(rlv, rjt){
 				// 返却
 				console.log('standby.');

ffb2c24ff6899c505379c84343f7a6ba1a9109c7
Tomoya Koyanagi <tomk79@gmail.com>
2016-06-12 15:49:27 +0900

    biflora 通信開通 他
    - 開発環境立ち上げるのが面倒なので https → http

--- src/board/board_files/scripts/app.js
+++ src/board/board_files/scripts/app.js
@@ -1,11 +1,63 @@
 window.app = new (function(){
 	// app "board"
-	var socket = this.socket = window.biflora
-		.createSocket(
-			new (function(){})(),
-			io,
-			{}
-		)
-	;
+	var _this = this;
+	var Promise = require('es6-promise').Promise;
+	var utils79 = require('utils79');
+	var it79 = require('iterate79');
+	var socket;
+
+	/**
+	 * 初期化
+	 */
+	this.init = function(callback){
+		callback = callback || function(){};
+
+		new Promise(function(rlv){rlv();})
+			.then(function(){ return new Promise(function(rlv, rjt){
+				// init biflora framework
+				socket = _this.socket = window.biflora
+					.createSocket(
+						new (function(){
+
+						})(),
+						io,
+						{
+							'showSocketTest': function( data, callback, main, socket ){
+								console.log(data);
+								// alert(data.message);
+								console.log(callback);
+								callback(data);
+								return;
+							}
+						}
+					)
+				;
+				rlv();
+			}); })
+			.then(function(){ return new Promise(function(rlv, rjt){
+				// (biflora 送信テスト)
+				socket.send(
+					'api1',
+					{
+					} ,
+					function(rtn){
+						console.log(rtn);
+						rlv();
+					}
+				);
+			}); })
+			// .then(function(){ return new Promise(function(rlv, rjt){
+			// 	rlv();
+			// }); })
+			.then(function(){ return new Promise(function(rlv, rjt){
+				// 返却
+				console.log('standby.');
+				callback(rtn);
+				rlv();
+			}); })
+		;
+
+		return;
+	}
 
 })();
